635d7650b9f768b98264cc4cb34b565e6c36ecb8
Fenhl <fenhl@fenhl.net>
2023-12-01 13:52:23 +0000

    Day 1, 2023: Replace regex lookups with hand-written parsing

--- crate/aoc2023/src/day01.rs
+++ crate/aoc2023/src/day01.rs
@@ -1,21 +1,5 @@
 #[allow(unused)] use aoc::prelude::*;
 
-fn parse_digit(digit: &str) -> u32 {
-    match digit {
-        "0" => 0,
-        "1" | "one" => 1,
-        "2" | "two" => 2,
-        "3" | "three" => 3,
-        "4" | "four" => 4,
-        "5" | "five" => 5,
-        "6" | "six" => 6,
-        "7" | "seven" => 7,
-        "8" | "eight" => 8,
-        "9" | "nine" => 9,
-        _ => panic!(),
-    }
-}
-
 #[aoc_generator(day1)]
 fn gen(input: &str) -> Vec<String> {
     input.lines().map(|line| line.to_owned()).collect()
@@ -37,10 +21,53 @@ fn part1(input: &[String]) -> u32 {
 fn part2(input: &[String]) -> u32 {
     input.par_iter()
         .map(|line| {
-            let first = regex_find!("[0-9]|one|two|three|four|five|six|seven|eight|nine", line).unwrap();
-            let (_, last) = regex_captures!(".*([0-9]|one|two|three|four|five|six|seven|eight|nine)", line).unwrap();
-            let tens = parse_digit(first);
-            let ones = if last.is_empty() { tens } else { parse_digit(last) };
+            let tens = line.char_indices()
+                .find_map(|(idx, c)| match c {
+                    '0'..='9' => c.to_digit(10),
+                    'o' => line.get(idx + 1..idx + 3).filter(|&rest| rest == "ne").map(|_| 1),
+                    't' => match line.get(idx + 1..idx + 3) {
+                        Some("wo") => Some(2),
+                        Some("hr") => line.get(idx + 3..idx + 5).filter(|&rest| rest == "ee").map(|_| 3),
+                        _ => None,
+                    },
+                    'f' => match line.get(idx + 1..idx + 4) {
+                        Some("our") => Some(4),
+                        Some("ive") => Some(5),
+                        _ => None,
+                    },
+                    's' => match line.get(idx + 1..idx + 3) {
+                        Some("ix") => Some(6),
+                        Some("ev") => line.get(idx + 3..idx + 5).filter(|&rest| rest == "en").map(|_| 7),
+                        _ => None,
+                    },
+                    'e' => line.get(idx + 1..idx + 5).filter(|&rest| rest == "ight").map(|_| 8),
+                    'n' => line.get(idx + 1..idx + 4).filter(|&rest| rest == "ine").map(|_| 9),
+                    _ => None,
+                })
+                .unwrap();
+            let ones = line.char_indices()
+                .rev()
+                .find_map(|(idx, c)| match c {
+                    '0'..='9' => c.to_digit(10),
+                    'e' => if let Some(start) = idx.checked_sub(2) {
+                        match &line[start..idx] {
+                            "on" => Some(1),
+                            "re" => idx.checked_sub(4).filter(|&start| &line[start..start + 2] == "th").map(|_| 3),
+                            "iv" => idx.checked_sub(3).filter(|&start| &line[start..start + 1] == "f").map(|_| 5),
+                            "in" => idx.checked_sub(3).filter(|&start| &line[start..start + 1] == "n").map(|_| 9),
+                            _ => None,
+                        }
+                    } else {
+                        None
+                    },
+                    'o' => idx.checked_sub(2).filter(|&start| &line[start..idx] == "tw").map(|_| 2),
+                    'r' => idx.checked_sub(3).filter(|&start| &line[start..idx] == "fou").map(|_| 4),
+                    'x' => idx.checked_sub(2).filter(|&start| &line[start..idx] == "si").map(|_| 6),
+                    'n' => idx.checked_sub(4).filter(|&start| &line[start..idx] == "seve").map(|_| 7),
+                    't' => idx.checked_sub(4).filter(|&start| &line[start..idx] == "eigh").map(|_| 8),
+                    _ => None,
+                })
+                .unwrap();
             10 * tens + ones
         }).sum()
 }
